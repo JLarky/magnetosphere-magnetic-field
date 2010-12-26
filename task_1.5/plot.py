f234ebd50084f8cd4616f7e8906945e72ca7c9d0
JLarky <jlarky@gmail.com>
2010-12-26 12:01:24 +0300

    Добавил изображение и код визуализации.

--- task_1.5/plot.py
+++ task_1.5/plot.py
@@ -1,77 +1,46 @@
 #!/usr/bin/python
 
-#from scipy.interpolate import griddata
 from matplotlib.mlab import griddata
-from numpy import *
-from numpy.random import uniform, seed
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.random import uniform, seed
-import numpy.ma as ma
-
 
 def plotone(x,y,z):
-    z = np.maximum( z, 0.001 )
-    z = np.minimum( z, 40 )
+    z = np.maximum( z, 0.001); z = np.minimum( z, 40 );
 
-    xi = linspace(-180,180, 200)
-    yi = linspace(-90,90, 100)
-#zi = griddata((x, y), z, (xi[None,:], yi[:,None]), method='cubic')
+    xi = np.linspace(-180,180, 200)
+    yi = np.linspace(-90,90, 100)
     zi = griddata(x, y, z, xi, yi)
 
-    print zi
-    
-    if True:
-        CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
-        CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.jet)
-        plt.xlim(np.min(xi),np.max(xi))
-        plt.ylim(np.min(yi),np.max(yi))
-        plt.colorbar() # draw colorbar
-    #plt.scatter(x,y,marker='o',c='b',s=5)
-    else:
-        v = 100
-    #plt.imshow(zi,vmin=-v,vmax=v)
-        plt.imshow(zi, vmin=0)
-        plt.colorbar() # draw colorbar
+    CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
+    CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.jet)
+    plt.xlim(np.min(xi),np.max(xi))
+    plt.ylim(np.min(yi),np.max(yi))
+    plt.colorbar() # draw colorbar
 
 
 plt.figure(num=None, figsize=(12, 10), dpi=90, facecolor='w', edgecolor='k') 
 
-data = genfromtxt("data.dat")
+data = np.genfromtxt("data.dat")
 
 x = data[:,2]
 y = data[:,1]
 
-plt.subplot(3, 2, 1)
-z = data[:,3]
-plotone(x,y,z)
+plt.subplot(3, 2, 1);plotone(x,y,data[:,3])
 plt.title('80-250KEV_PRTN(0_DG)')
 
-plt.subplot(3, 2, 2)
-z = data[:,4]
-plotone(x,y,z)
+plt.subplot(3, 2, 2);plotone(x,y,data[:,4])
 plt.title('250-800KEV_PRTN(0_DG)')
 
-plt.subplot(3, 2, 3)
-z = data[:,5]
-plotone(x,y,z)
+plt.subplot(3, 2, 3);plotone(x,y,data[:,5])
 plt.title('800-2500KEV_PRTN(0_DG)')
 
-plt.subplot(3, 2, 4)
-z = data[:,6]
-plotone(x,y,z)
+plt.subplot(3, 2, 4);plotone(x,y,data[:,6])
 plt.title('>2500KEV_PRTN(0_DG)')
 
-plt.subplot(3, 2, 5)
-z = data[:,7]
-plotone(x,y,z)
+plt.subplot(3, 2, 5);plotone(x,y,data[:,7])
 plt.title('>100KEV_ELEC(0_DG)')
 
-plt.subplot(3, 2, 6)
-z = data[:,8]
-plotone(x,y,z)
+plt.subplot(3, 2, 6);plotone(x,y,data[:,8])
 plt.title('>300KEV_ELEC(0_DG)')
 
-
 plt.savefig("energies.png")
-#plt.show()
